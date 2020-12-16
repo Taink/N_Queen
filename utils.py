64c54ff95a4700108158a0cccfa799600175fc40
Saibe <sebastien.cuvellier@hotmail.com>
2020-12-16 11:10:35 +0100

    ✨Add print board

---
+++ utils.py
@@ -0,0 +1,13 @@
+
+
+def print_board(size, board):
+    for i in range(size):
+        for j in range(size):
+            print(" " + board[i][j])
+        print("\n")
+
+def can_t_attack(size, board):
+    pass  # TODO
+
+def is_soluce(size, board):
+    pass  # TODO
