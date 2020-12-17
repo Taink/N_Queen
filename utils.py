656d64305815efa951dbf75d7d8e6236e342b61b
Taink <minemax579@gmail.com>
2020-12-17 03:33:45 +0100

    🚧 Add helper methods for can_t_attack method

--- utils.py
+++ utils.py
@@ -1,3 +1,24 @@
+# import math
+
+
+def is_diagonal(coord_a: tuple[int, int], coord_b: tuple[int, int]):
+    x1, y1 = coord_a
+    x2, y2 = coord_b
+    return abs(x1 - x2) == abs(y1 - y2)
+
+
+def is_attacking(coord_a: tuple[int, int], coord_b: tuple[int, int]):
+    x1, y1 = coord_a
+    x2, y2 = coord_b
+    return (x1 == x2) or (y1 == y2) or is_diagonal(coord_a, coord_b)
+
+
+def has_attacking(queen_locations: list[tuple[int, int]]):
+    for i, q in enumerate(queen_locations):
+        for q2 in queen_locations[i + 1:]:
+            if is_attacking(q, q2):
+                return True
+    return False
 
 
 def print_board(size, board):
@@ -6,8 +27,10 @@ def print_board(size, board):
             print(" " + board[i][j])
         print("\n")
 
+
 def can_t_attack(size, board):
     pass  # TODO
+
 
 def is_soluce(size, board):
     pass  # TODO
