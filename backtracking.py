bd26303b6c090372ed0ac7a121143ae71a78c5ac
Taink <minemax579@gmail.com>
2020-12-29 18:32:59 +0100

    🎨 Improve format of backtracking script

--- backtracking.py
+++ backtracking.py
@@ -1,18 +1,20 @@
 from utils import print_board, can_t_attack, is_soluce
 
+
 def solve_n_queen_small(board_size: int, board):
     return board, solve_n_queen_small_helper(board_size, board, 0)
+
 
 def solve_n_queen_small_helper(board_size, board, line):
     if line >= board_size:
         return True
-    
+
     for y in range(board_size):
         board[y][line] = 1
         if not can_t_attack(board_size, board):
             board[y][line] = 0
         else:
-            if solve_n_queen_small_helper(board_size, board, line + 1) == True:
+            if solve_n_queen_small_helper(board_size, board, line + 1):
                 return True
         board[y][line] = 0
     return False
