b978afffb31ccc6b27045a95a4d680142ee2d1d6
Saibe <sebastien.cuvellier@hotmail.com>
2021-01-04 19:19:08 +0100

    🎨 Format and comment

--- backtracking.py
+++ backtracking.py
@@ -1,11 +1,19 @@
 from utils import print_board, can_t_attack, is_soluce
 
 
-def solve_n_queen_small(board_size: int, board):
+def solve_n_queen_small(board_size: int, board: list[list[int]]) -> tuple[list[list[int]], bool]:
     return board, solve_n_queen_small_helper(board_size, board, 0)
 
 
-def solve_n_queen_small_helper(board_size, board, line):
+def solve_n_queen_small_helper(board_size: int, board: list[list[int]], line: int) -> bool:
+    """
+    Look on a line to position a queen then recursive call
+
+    :param board_size: Size of the board
+    :param board: Chess board (0 is void, 1 is queen)
+    :param line: current line
+    :return: If the board is one of the solutions
+    """
     if line >= board_size:
         return True
 
